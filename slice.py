98e3212179c4f99eb490f010b64ce0823fc89281
Sam Genduso <sam.genduso@gmail.com>
2015-11-29 05:04:30 -0700

    slice

---
+++ slice.py
@@ -0,0 +1,20 @@
+example [0,1,2,3,4,5,6,7,8,9]
+exmaple[4:8]
+example[4:9]
+example[4,10]
+example[-5:-1]
+example[-5:-0]
+example[-5:]
+#leaving it empty will mean continue to the end
+example[:7]
+example[:]
+
+example[1:8:2]
+
+# go by 2's
+
+example[10:0:-2]
+
+#backwards
+
+example[::-2]
