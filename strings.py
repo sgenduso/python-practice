beed1d8bc34ef1ce53eb4f9170dee1e10b4cb0e8
Sam Genduso <sam.genduso@gmail.com>
2015-11-30 03:33:58 -0700

    strings

---
+++ strings.py
@@ -0,0 +1,27 @@
+"hey now"
+
+'he\'s a jerk'
+
+a = "cam"
+b = "buckingham"
+
+a+b
+a b
+a,b
+
+a = "cam "
+a+b
+
+
+num = 18
+num + 16
+
+print "Buck " +num
+numb = str(18)
+print"Buck is "+num
+num2 = 32
+print "my mom is " + num2
+print "my mom is " + `num2`
+
+#backticks to convert to String
+#repr() = str()
