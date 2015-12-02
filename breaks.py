c81c441c16c7ac67eb66b32a61795b99171eb466
Sam Genduso <sam.genduso@gmail.com>
2015-12-02 08:08:20 -0700

    breaks

---
+++ breaks.py
@@ -0,0 +1,20 @@
+magicNumber = 26
+
+print ("bucky" + 9)
+print ("bucky", 9)
+
+for n in range (101):
+    if n is magicNumber:
+        print("Yea!")
+        break
+        ##breaks out or stops your loop
+    else:
+        print(n)
+
+
+##challenge
+
+for n in range (100):
+    if n % 4 is 0:
+        print(n, "is devisable by 4")
+
