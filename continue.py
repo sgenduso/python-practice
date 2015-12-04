c273a668927c8f74ddba726172b93f5e43399c59
Sam Genduso <sam.genduso@gmail.com>
2015-12-04 08:15:31 -0700

    continue

---
+++ continue.py
@@ -0,0 +1,8 @@
+numbersTaken = [2,5,12,33,17]
+
+print("Here are the numbers that are still available:")
+ for n in range (1,20):
+     if n in numbersTaken
+     continue
+     ##breaks out of the loop completely, continue skips whatever is after it
+     print(n)
