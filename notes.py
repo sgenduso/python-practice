e288afd4370b5c2ac3e7a6e7809d1bc7da791065
Sam Genduso <sam.genduso@gmail.com>
2016-04-19 18:48:59 -0600

    created notes doc for reference

---
+++ notes.py
@@ -0,0 +1,21 @@
+# Variable naming conventions
+# snake_case
+# no 'var', $, etc
+
+# Whitespace
+# Bad:
+def spam():
+eggs = 12
+return eggs
+print spam()
+
+# Good:
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
+
+"""Multi
+   Line
+   Comments"""
