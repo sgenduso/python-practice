32d4788514ae8db1343b71b0175cded66ae871f4
Sam Genduso <sam.genduso@gmail.com>
2016-04-23 09:17:15 -0600

    explicit string conversion

--- src/string_methods.py
+++ src/string_methods.py
@@ -14,3 +14,11 @@ print str(pi)
 
 # concatenation
 print "Spam " + "and " + "eggs"
+
+# explicit string conversion
+# BAD:
+print "The value of pi is around " + 3.14
+# GOOD:
+print "The value of pi is around " + str(3.14)
+
+# string formatting with %
