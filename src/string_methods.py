00e1a6a87969f8b3414bf074218cce815e89ad29
Sam Genduso <sam.genduso@gmail.com>
2016-04-24 09:26:12 -0600

    string formatting with %

--- src/string_methods.py
+++ src/string_methods.py
@@ -22,3 +22,12 @@ print "The value of pi is around " + 3.14
 print "The value of pi is around " + str(3.14)
 
 # string formatting with %
+string_1 = "Camelot"
+string_2 = "place"
+print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
+
+name = raw_input("What is your name?")
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
