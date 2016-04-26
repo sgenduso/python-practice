6709fcc6b86b33c0fa04a5df007a590b94457c92
Sam Genduso <sam.genduso@gmail.com>
2016-04-25 18:41:05 -0600

    comparators

---
+++ src/comparators.py
@@ -0,0 +1,17 @@
+# Equal to (==)
+bool_equal = 100 == (2*50) # True
+
+# Not equal to (!=)
+bool_not_equal = 99 != (98+1) # False
+
+# Less than (<)
+bool_less_than = 17 < 328 # True`
+
+# Less than or equal to (<=)
+bool_lte = 19 <= 19 # True
+
+# Greater than (>)
+bool_greater_than = 17 > 328 # False
+
+# Greater than or equal to (>=)
+bool_gte = -22 >= -18 # False
