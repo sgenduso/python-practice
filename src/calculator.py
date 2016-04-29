dd5eb359ebe6b21015bc18a31e64ff30f3cd69f8
Sam Genduso <sam.genduso@gmail.com>
2016-04-28 21:05:36 -0600

    updated calculator

--- src/calculator.py
+++ src/calculator.py
@@ -1,7 +1,15 @@
-meal = 44.50
-tax = 6.75 / 100
-tip = 15.0 / 100
-meal = meal + meal * tax
-total = meal + meal * tip
+def tax(bill):
+    """Adds 8% tax to a restaurant bill."""
+    bill *= 1.08
+    print "With tax: %f" % bill
+    return bill
 
-print("%.2f" % total)
+def tip(bill):
+    """Adds 15% tip to a restaurant bill."""
+    bill *= 1.15
+    print "With tip: %f" % bill
+    return bill
+
+meal_cost = 100
+meal_with_tax = tax(meal_cost)
+meal_with_tip = tip(meal_with_tax)
