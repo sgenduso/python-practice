fc81b84682f5ae74816776f6071ce5900d52c2d2
Sam Genduso <sam.genduso@gmail.com>
2016-05-02 18:05:33 -0600

    math module

---
+++ src/math.py
@@ -0,0 +1,7 @@
+import math
+print math.sqrt(25)
+
+# so you don't have to keep typing 'math'
+# from math import *
+
+# ['__doc__', '__name__', '__package__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'hypot', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'modf', 'pi', 'pow', 'radians', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'trunc']
