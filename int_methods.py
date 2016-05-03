97d386c790549ef2de1a6b5070c596e31764358e
Sam Genduso <sam.genduso@gmail.com>
2016-05-03 17:36:46 -0600

    int methods

---
+++ int_methods.py
@@ -0,0 +1,7 @@
+# maximum - best to use only with ints and floats
+print max(1,2,3) # 3
+
+# minimum - best to use only with ints and floats
+print min(1,2,3) # 1
+
+# absolute value
---
+++ src/other_methods.py
@@ -0,0 +1,4 @@
+#type
+print type(1) # int
+print type(1.2) # float
+print type('1') # string
