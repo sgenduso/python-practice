86eafec97166afb6bf9917bf5a1967521ebe5482
Sam Genduso <sam.genduso@gmail.com>
2016-05-05 21:56:06 -0600

    list append and slice fns

---
+++ src/list_methods.py
@@ -0,0 +1,21 @@
+park = []
+park.append("golden")
+
+park.append("husky")
+park.append("shepherd")
+park.append("lab")
+
+# same as:
+park2 = ["golden", "husky", "shepherd", "lab"]
+
+list_length = len(park)
+
+print "There are %d items in the park." % (list_length)
+print park
+
+# slice: We start at the index before the colon and continue up to but not including the index after the colon.
+suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
+
+first  = suitcase[0:2]  # The first and second items (index zero and one)
+middle =  suitcase[2:4] # Third and fourth items (index two and three)
+last   = suitcase[4:6]  # The last two items (index four and five)
