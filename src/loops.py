ea03efe12c1bb3315feb6eeb06965763df72c986
Sam Genduso <sam.genduso@gmail.com>
2016-05-12 20:55:38 -0600

    loop with sort

--- src/loops.py
+++ src/loops.py
@@ -2,3 +2,14 @@ my_list = [1,9,3,8,5,7]
 
 for number in my_list:
     print 2 * number
+
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+for number in start_list:
+    square_list.append(number **2)
+
+square_list.sort()
+
+
+print square_list
