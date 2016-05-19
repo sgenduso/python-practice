3d3095d9476917fa56996602783e1f51d418df92
Sam Genduso <sam.genduso@gmail.com>
2016-05-19 17:23:32 -0600

    student averages

--- src/student_teacher.py
+++ src/student_teacher.py
@@ -16,3 +16,16 @@ tyler = {
     "quizzes": [0.0, 75.0, 78.0],
     "tests": [100.0, 100.0]
 }
+
+students = [lloyd, alice, tyler]
+
+for student in students:
+    print student['name']
+    print student['homework']
+    print student['quizzes']
+    print student['tests']
+
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+    return total/len(numbers)
