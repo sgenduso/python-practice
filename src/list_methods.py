23855da38e2e1afd50018432a70912e87b339356
Sam Genduso <sam.genduso@gmail.com>
2016-05-26 11:32:34 -0400

    range fn

--- src/list_methods.py
+++ src/list_methods.py
@@ -50,3 +50,13 @@ del(n[1]) # removes 3 but does not return it
 def print_list(x):
     for i in range(0, len(x)):
         print x[i]
+
+# The range function has three different versions:
+# range(stop)
+# range(start, stop)
+# range(start, stop, step)
+# In all cases, the range() function returns a list of numbers from start up to (but not including) stop. Each item increases by step.
+# If omitted, start defaults to zero and step defaults to one.
+range(6) # => [0,1,2,3,4,5]
+range(1,6) # => [1,2,3,4,5]
+range(1,6,3) # => [1,4]
